b222d820191a811064e79d74d17fa17183306837
edhaase <edward@TITAN.localdomain>
2017-08-23 18:20:32 -0700

    Fix: Unsupport phi use of let in Market

--- Market.js
+++ Market.js
@@ -7,13 +7,42 @@ module.exports = {
 	 * @todo Include sender/recipient (recipient isn't always provided)
 	 */
 	updateMarket: function (freq = 5) {
-		var incoming = Game.market.incomingTransactions;
-		var outgoing = Game.market.outgoingTransactions;
+		this.updateIncomingMarket(freq);
+		this.updateOutgoingMarket(freq);
+	},
 
-		/**
-		 * Incoming notifications		 
-		 */
-		var transaction, i, j, len;
+	/**
+	 * Outgoing notifications
+	 */
+	updateOutgoingMarket: function(freq = 5) {
+		var outgoing = Game.market.outgoingTransactions;
+		var transaction, i, j, len, total, distance;
+		for (j = 0, len = outgoing.length; j < len; j++) {
+			transaction = outgoing[j];
+			if (Game.time - transaction.time > freq)
+				break;
+			if (transaction.to && _.get(Game.rooms, transaction.to + '.controller.my', false) === true)
+				continue;
+			let { from, to, resourceType, amount, order, recipient } = transaction;
+			distance = Game.map.getRoomLinearDistance(from, to, true);
+			if (order && order.price) {
+				total = amount * order.price;
+				Game.rooms[from].terminal.credits += (total * (1.0 - TERMINAL_TAX));
+				Log.info(`Outbound transaction from ${from} to ${to} (dist: ${distance}): ${amount} ${resourceType} at ${order.price} for ${total} total`, 'Market');
+			} else {
+				Log.info(`Outbound transaction from ${from} to ${to} (dist: ${distance}): ${amount} ${resourceType}`, 'Market');
+			}
+			if (recipient && recipient.username)
+				this.updateRecipient(recipient.username, to, transaction);
+		}
+	},
+
+	/**
+	 * Incoming notifications
+	 */
+	updateIncomingMarket: function(freq = 5) {
+		var incoming = Game.market.incomingTransactions;
+		var transaction, i, j, len, total, distance, desc;
 		for (i = 0, len = incoming.length; i < len; i++) {
 			transaction = incoming[i];
 			if (Game.time - transaction.time > freq)
@@ -21,11 +50,11 @@ module.exports = {
 			let { from, to, resourceType, amount, order, sender } = transaction;
 			if (transaction.from && _.get(Game.rooms, transaction.from + '.controller.my', false) === true)
 				continue;
-			let distance = Game.map.getRoomLinearDistance(transaction.from, transaction.to, true);
+			distance = Game.map.getRoomLinearDistance(transaction.from, transaction.to, true);
 			// var price = (order.price) ? order.price : 'NA';
-			var desc = _.escape(transaction.description);
+			desc = _.escape(transaction.description);
 			if (order && order.price) {
-				let total = amount * order.price;
+				total = amount * order.price;
 				Game.rooms[to].terminal.credits -= total;
 				Log.info(`Inbound transaction to ${to} from ${from} (dist: ${distance}): ${amount} ${resourceType} at ${order.price} for ${total} total (${desc})`, 'Market');
 			} else {
@@ -34,28 +63,6 @@ module.exports = {
 			if (sender && sender.username)
 				this.updateSender(sender.username, from, transaction);
 			// Game.notify("Transaction received: " + JSON.stringify(transaction,null,2));						
-		}
-
-		/**
-		 * Outgoing notifications
-		 */
-		for (j = 0, len = outgoing.length; j < len; j++) {
-			transaction = outgoing[j];
-			if (Game.time - transaction.time > freq)
-				break;
-			if (transaction.to && _.get(Game.rooms, transaction.to + '.controller.my', false) === true)
-				continue;
-			let { from, to, resourceType, amount, order, recipient } = transaction;
-			let distance = Game.map.getRoomLinearDistance(from, to, true);
-			if (order && order.price) {
-				let  total = amount * order.price;
-				Game.rooms[from].terminal.credits += (total * (1.0 - TERMINAL_TAX));
-				Log.info(`Outbound transaction from ${from} to ${to} (dist: ${distance}): ${amount} ${resourceType} at ${order.price} for ${total} total`, 'Market');
-			} else {
-				Log.info(`Outbound transaction from ${from} to ${to} (dist: ${distance}): ${amount} ${resourceType}`, 'Market');
-			}
-			if (recipient && recipient.username)
-				this.updateRecipient(recipient.username, to, transaction);
 		}
 	},
 
