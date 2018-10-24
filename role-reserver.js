c0cdf665d970a403a7b33038f06cff7e828c2665
Edward Haase <edward.r.haase@gmail.com>
2018-10-24 09:00:04 -0700

    Reservers now clear room messages

--- role-reserver.js
+++ role-reserver.js
@@ -12,22 +12,22 @@ module.exports = {
 	priority: function () {
 		// (Optional)
 	},
-	minBody: [CLAIM,CLAIM,MOVE,MOVE],
-	minCost: UNIT_COST([CLAIM,CLAIM,MOVE,MOVE]),
-	body: function() {
+	minBody: [CLAIM, CLAIM, MOVE, MOVE],
+	minCost: UNIT_COST([CLAIM, CLAIM, MOVE, MOVE]),
+	body: function () {
 		// (Optional) Used if no body supplied
 		// Expects conditions..
 	},
 	init: function () {
 		this.pushStates([
-			['EvadeMove', {pos: this.memory.site, range: 1}]
+			['EvadeMove', { pos: this.memory.site, range: 1 }]
 		]);
 	},
 	/* eslint-disable consistent-return */
 	run: function () {
 		const { controller } = this.room;
 		if (!controller || this.pos.roomName !== this.memory.site.roomName)
-			return this.pushState('EvadeMove', {pos: this.memory.site, range: 1});
+			return this.pushState('EvadeMove', { pos: this.memory.site, range: 1 });
 		this.room.memory.reservation = Game.time + _.get(controller, 'reservation.ticksToEnd', 0);
 		if (this.flee(10) === OK)
 			return;
@@ -41,5 +41,7 @@ module.exports = {
 			status = this.reserveController(controller);
 		if (status === ERR_NOT_IN_RANGE)
 			this.moveTo(controller, { range: 1 });
+		else if (status === OK && controller && controller.sign && controller.sign.text)
+			this.signController(controller, '');
 	}
 };
