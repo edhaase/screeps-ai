bc6e6cfaa626e02ac525623e4893fbfac39ffbaa
Edward Haase <edward.r.haase@gmail.com>
2019-04-08 17:34:43 -0700

    xorcrypt only exports the one function

--- os.algo.xorcrypt.js
+++ os.algo.xorcrypt.js
@@ -2,7 +2,7 @@
 'use strict';
 
 /* Appropriated from Semperrrabit */
-exports.xorcrypt = function xorcrypt(str, key) {
+module.exports = function xorcrypt(str, key) {
 	var ret = "";
 	for (var i = 0; i < str.length; i++) {
 		ret += String.fromCharCode(str.charCodeAt(i) ^ key.charCodeAt(i % key.length));
