58fc80fd2b5f063f5f633d79d14f673a163ae361
Edward Haase <edward.r.haase@gmail.com>
2019-04-08 18:52:46 -0700

    Move map enclosure test to algo

--- Algo.js
+++ Algo.js
@@ -402,36 +402,6 @@ function findRouteToHighway(roomName, rejector = (name) => false) {
 }
 
 /**
- * Check if a room is reachable from the world
- * Algo.isRoomEnclosed('W7N2', {ignore: r => Game.rooms[r] && Game.rooms[r].my})
- * _.filter(Game.rooms, r => r.my && !Algo.isRoomEnclosed(r.name));
- * [room W7N3],[room W7N4],[room W8N4]
- */
-function isRoomEnclosed(start, opts = {}) {
-	_.defaults(opts, {
-		ignore: r => Game.rooms[r] && Game.rooms[r].my,
-		avoid: (r, EW, NS) => Game.rooms[r] && Game.rooms[r].owned && !Game.rooms[r].my
-	})
-	var exit, roomName, rooms = [start];
-	for (var i = 0; i < rooms.length; i++) {
-		roomName = rooms[i];
-		// console.log('Checking ' + roomName);
-		var [, EW, NS] = /[EW](\d+)[NS](\d+)/.exec(roomName);
-		// Center or highway
-		if ((EW % 10 == 0 || NS % 10 == 0) || (EW % 10 == 5 && NS % 10 == 5)
-			|| (opts.avoid && opts.avoid(roomName, EW, NS)))
-			return false;
-		var neighbors = _.values(Game.map.describeExits(roomName));
-		while (exit = neighbors.shift()) {
-			if (rooms.includes(exit) || (opts.ignore && opts.ignore(exit)))
-				continue;
-			rooms.push(exit);
-		}
-	}
-	return true;
-}
-
-/**
  * MST of a room from starting position
  * example: Algo.prim(new RoomPosition(15,30,'W2N7'))
  * example: Algo.prim(new Algo.RoomGraph('W2N7'),15,29)
---
+++ os.algo.map.enclosure.js
@@ -0,0 +1,33 @@
+/** os.algo.map.enclosure */
+'use strict';
+/**
+ * Check if a room is reachable from the world.
+ * 
+ * Algo.isRoomEnclosed('W7N2', {ignore: r => Game.rooms[r] && Game.rooms[r].my})
+ * _.filter(Game.rooms, r => r.my && !Algo.isRoomEnclosed(r.name));
+ * [room W7N3],[room W7N4],[room W8N4]
+ */
+
+exports.isRoomEnclosed = function isRoomEnclosed(start, opts = {}) {
+	_.defaults(opts, {
+		ignore: r => Game.rooms[r] && Game.rooms[r].my,
+		avoid: (r, EW, NS) => Game.rooms[r] && Game.rooms[r].owned && !Game.rooms[r].my
+	});
+	var exit, roomName, rooms = [start];
+	for (var i = 0; i < rooms.length; i++) {
+		roomName = rooms[i];
+		// console.log('Checking ' + roomName);
+		var [, EW, NS] = /[EW](\d+)[NS](\d+)/.exec(roomName);
+		// Center or highway
+		if ((EW % 10 === 0 || NS % 10 === 0) || (EW % 10 === 5 && NS % 10 === 5)
+			|| (opts.avoid && opts.avoid(roomName, EW, NS)))
+			return false;
+		var neighbors = _.values(Game.map.describeExits(roomName));
+		while ((exit = neighbors.shift())) {
+			if (rooms.includes(exit) || (opts.ignore && opts.ignore(exit)))
+				continue;
+			rooms.push(exit);
+		}
+	}
+	return true;
+};
