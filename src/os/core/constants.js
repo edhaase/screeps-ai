d957aa78e2550fee0b836f2903f36374a3835e3d
Edward Haase <edward.r.haase@gmail.com>
2019-06-08 09:52:37 -0700

    Update OS constants

--- src/os/core/constants.js
+++ src/os/core/constants.js
@@ -14,14 +14,13 @@ global.THP_SEGMENT_INTEL = 5; // Intel gathering (Span 3 = Segment 15-17)
 
 global.BUCKET_MAX = ENV('BUCKET_MAX', 10000);
 
-// MAKE_CONSTANT(global, 'RUNTIME_ID', Game.time);
-global.RUNTIME_ID = Game.time;
-global.IS_PTR = !!(Game.shard && Game.shard.ptr);
-global.IS_SIM = !!Game.rooms['sim'];
-global.IS_MMO = !!(Game.shard && Game.shard.name && Game.shard.name.startsWith('shard'));
+MAKE_CONSTANT(global, 'RUNTIME_ID', Game.time);
+MAKE_CONSTANT(global, 'IS_PTR', !!(Game.shard && Game.shard.ptr));
+MAKE_CONSTANT(global, 'IS_SIM', !!Game.rooms['sim']);
+MAKE_CONSTANT(global, 'IS_MMO', !!(Game.shard && Game.shard.name && Game.shard.name.startsWith('shard')));
 
-global.INVADER_USERNAME = 'Invader';
-global.SOURCE_KEEPER_USERNAME = 'Source Keeper';
+MAKE_CONSTANT(global, 'INVADER_USERNAME', 'Invader');
+MAKE_CONSTANT(global, 'SOURCE_KEEPER_USERNAME', 'Source Keeper');
 
 MAKE_CONSTANT(global, 'PROCESS_NAMESPACE', 'os.prog.');
 MAKE_CONSTANT(global, 'SHARD_TOKEN', (Game.shard && Game.shard.name && Game.shard.name.slice(-1)) || '');
