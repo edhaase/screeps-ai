1ba28f424ba0327504033c86cab5e1358b97f7e0
Edward Haase <edward.r.haase@gmail.com>
2019-10-26 14:29:46 -0700

    Clean up attacker

--- src/role/attack.js
+++ src/role/attack.js
@@ -1,25 +1,27 @@
 /**
- * role.attacker.js
- *
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.attack');
- * mod.thing == 'a thing'; // true
+ * role.attacker.js - Pretty much identical to the bulldozer, with some additional checks.
  */
 'use strict';
 
 module.exports = {
-	boosts: ['ZH', 'ZH2O', 'XZH2O'],
+	//  boosts: ['ZH', 'ZH2O', 'XZH2O'],
 	priority: function () {
 		// (Optional)
 	},
-	body: function ({ room }) {
-		return Arr.repeat([WORK, MOVE], room.energyAvailable);
+	body: function ({room}) {
+		return Arr.repeat([ATTACK, MOVE], room.energyAvailable);
+	},
+	init: function () {
+		this.pushState("TowerDrain", { dest: this.memory.dest });
+		this.pushState("MoveToRoom", { room: this.memory.dest, enter: false });
 	},
 	/* eslint-disable consistent-return */
 	run: function () {
+		if (this.pos.roomName !== this.memory.dest)
+			return this.pushState('MoveToRoom', { room: this.memory.dest, enter: true });
+		if (this.room.controller.safeMode && this.room.controller.safeMode > this.ticksToLive)
+			return this.setRole('recycle');
+
 		const { avoidRamparts = true } = this.memory;
 		const target = this.getTarget(
 			({ room }) => room.find(FIND_HOSTILE_STRUCTURES, { filter: s => s.structureType !== STRUCTURE_CONTROLLER && s.structureType !== STRUCTURE_RAMPART }),
@@ -32,19 +34,24 @@ module.exports = {
 				this.memory.avoidRamparts = false;
 				return;
 			} else {
-				this.memory.avoidRamparts = true;
-				return this.pushState('Breach', this.pos.roomName);
+				this.setRole('recycle');
+				// this.memory.avoidRamparts = true;
+				// return this.pushState('Breach', this.pos.roomName);
 			}
 		}
-		const range = this.pos.getRangeTo(target);
-		if (range <= CREEP_RANGED_ATTACK_RANGE && this.hasActiveBodypart(RANGED_ATTACK))
+
+		// RMA doesn't block anything
+		if (this.hasActiveBodypart(RANGED_ATTACK))
 			this.rangedMassAttack();
+		const range = this.pos.getRangeTo(target);
 		if (range > 1) {
 			this.moveTo(target);
-			const wall = this
-		} else if (this.hasActiveBodypart(WORK))
+			this.heal(this);
+		} else if (this.hasActiveBodypart(WORK) && CONSTRUCTION_COST[target.structureType]) {
 			this.dismantle(target);
-		else if (this.hasActiveBodypart(ATTACK))
+		} else if (this.hasActiveBodypart(ATTACK))
 			this.attack(target);
+		else
+			this.heal(this);
 	}
 };
